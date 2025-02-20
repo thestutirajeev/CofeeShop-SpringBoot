16afd441cea7cb23bebca19e65129fbf0f3c4489
Stuti Rajeev <stutirajeev5@gmail.com>
2025-02-20 18:51:14 +0530

    Initial Commit

---
+++ src/main/java/com/CofeeShop/CofeeShopApplication.java
@@ -0,0 +1,13 @@
+package com.CofeeShop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CofeeShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CofeeShopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/CofeeShop/CofeeShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.CofeeShop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CofeeShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
